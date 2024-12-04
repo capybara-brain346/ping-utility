0fb828e81d2c9883974395110fa103e044db0b8e
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-04 18:19:09 +0000

    first trial

--- cmd/icmp_packet.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type icmpPacket struct {
-	Type     uint8
-	Code     uint8
-	Checksum uint16
-	ID       uint16
-	Seq      uint16
-}
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,76 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"net"
 	"os"
 	"time"
 )
+
+const (
+	icmpEchoRequest = 8
+	icmpEchoReply   = 0
+)
+
+type icmpPacket struct {
+	Type     uint8
+	Code     uint8
+	Checksum uint16
+	ID       uint16
+	Seq      uint16
+}
+
+func checksum(data []byte) uint16 {
+	sum := 0
+	for i := 0; i < len(data)-1; i += 2 {
+		sum += int(data[i])<<8 | int(data[i+1])
+	}
+	if len(data)%2 == 1 {
+		sum += int(data[len(data)-1])
+	}
+	sum = (sum >> 16) + (sum & 0xffff)
+	return uint16(^sum)
+}
+
+func sendPing(addr string, id, seq uint16) (time.Duration, error) {
+	conn, err := net.Dial("ip4:icmp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	packet := icmpPacket{
+		Type: icmpEchoRequest,
+		Code: 0,
+		ID:   id,
+		Seq:  seq,
+	}
+	buffer := make([]byte, 8)
+	binary.BigEndian.PutUint16(buffer[4:], id)
+	binary.BigEndian.PutUint16(buffer[6:], seq)
+	packet.Checksum = checksum(buffer)
+	binary.BigEndian.PutUint16(buffer[2:], packet.Checksum)
+
+	start := time.Now()
+	if _, err := conn.Write(buffer); err != nil {
+		return 0, err
+	}
+
+	reply := make([]byte, 1024)
+	conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	_, err = conn.Read(reply)
+	if err != nil {
+		return 0, err
+	}
+	elapsed := time.Since(start)
+
+	if reply[20] != icmpEchoReply {
+		return 0, fmt.Errorf("did not receive echo reply")
+	}
+
+	return elapsed, nil
+}
 
 func main() {
 	if len(os.Args) != 2 {
